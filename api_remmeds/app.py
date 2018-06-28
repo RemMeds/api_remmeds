a57c2741a2481adc71015d41fe12901412858c1a
klougit <quentin.nicolas.pro@gmail.com>
2018-06-28 11:49:37 +0200

    repertory list for a user

--- api_remmeds/app.py
+++ api_remmeds/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restplus import Api
 from api_remmeds.api.controllers.user_connexion_controller import ns as check_account
+from api_remmeds.api.controllers.repertory_controller import ns as check_repertory
 
 app = Flask(__name__)
 api = Api(app)
 api.add_namespace(check_account)
+api.add_namespace(check_repertory)
 
 if __name__ == "__main__":
     app.run(debug=True, host='192.168.1.26', port='15020')
