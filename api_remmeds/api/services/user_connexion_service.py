42e6275f095c20236906200ef713cd27b1afc7bf
klougit <quentin.nicolas.pro@gmail.com>
2018-06-28 16:43:12 +0200

    update profil + path changes

--- api_remmeds/api/services/user_connexion_service.py
+++ api_remmeds/api/services/user_connexion_service.py
@@ -31,3 +31,14 @@ def create_account(your_mail, your_password):
     cur.execute("INSERT INTO rm_user (us_mail, us_mdp) VALUES ('" + your_mail + "', '" + your_password + "')")
     db.commit()
     db.close()
+
+
+def update_account(your_mail, your_lastname, your_firstname, your_bf, your_lun, your_din, your_bed):
+    db = pymysql.connect(host='localhost', user='root', password='azerty', db='remmeds_users')
+    cur = db.cursor()
+    cur.execute(
+        "UPDATE rm_user SET us_lastname = '" + your_lastname + "', us_firstname = '" + your_firstname +
+        "', us_prefbreakfast = '" + your_bf + "', us_preflunch = '" + your_lun + "', us_prefdinner = '" +
+        your_din + "', us_prefbedtime = '" + your_bed + "' WHERE us_mail = '" + your_mail + "'")
+    db.commit()
+    db.close()
