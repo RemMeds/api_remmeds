5cd0957e4a6661f70547af9555547d7d08e3c4fa
klougit <quentin.nicolas.pro@gmail.com>
2018-07-03 23:54:20 +0200

    new historic path

--- api_remmeds/api/services/historic_service.py
+++ api_remmeds/api/services/historic_service.py
@@ -8,7 +8,7 @@ def list_historic(user_id):
     historic_list = []
     for req in cur.fetchall():
         historic = {
-            "medicine_name": req[2],
+            "drog_name": req[2],
             "hour": req[3],
             "day": req[4],
             "respected": req[5]
@@ -16,3 +16,12 @@ def list_historic(user_id):
         historic_list.append(historic)
     db.close()
     return historic_list
+
+
+def add_historic(user_id, drog_name, hour, day, intake_respected):
+    db = pymysql.connect(host='localhost', user='root', password='azerty', db='remmeds_users')
+    cur = db.cursor()
+    cur.execute(
+        "INSERT INTO rm_historic (us_id, hi_drugname, hi_hours, hi_day, hi_takenrespected) VALUES ('" + user_id + "', '" + drog_name + "', '" + hour + "', '" + day + "', '" + intake_respected + "')")
+    db.commit()
+    db.close()
