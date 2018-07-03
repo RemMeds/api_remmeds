c4d840c67f338b9fbbea7367421f6fb4808f1cec
klougit <quentin.nicolas.pro@gmail.com>
2018-07-03 23:54:49 +0200

    raspberry historic path

--- api_remmeds/api/controllers/raspberry_controller.py
+++ api_remmeds/api/controllers/raspberry_controller.py
@@ -1,12 +1,39 @@
 from flask_restplus import Resource, Namespace
+
+from api_remmeds.api.services.contact_service import get_user_contacts
 from api_remmeds.api.services.raspberry_service import get_user
+from api_remmeds.api.services.historic_service import list_historic, add_historic
 
 ns = Namespace('raspberry', description='Path needed for raspberry synchronization')
 
 
 @ns.route('/get_user/<user>')
-class ListContactController(Resource):
+class GetUserController(Resource):
     @staticmethod
     def get(user):
         result = get_user(user)
         return {"user": result}
+
+
+@ns.route('/list_historic/<user>')
+class GetHistoricController(Resource):
+    @staticmethod
+    def get(user):
+        result = list_historic(user)
+        return {"historic": result}
+
+
+@ns.route('/list_contact/<user>')
+class ListContactController(Resource):
+    @staticmethod
+    def get(user):
+        result = get_user_contacts(user)
+        return {"contact": result}
+
+
+@ns.route('/add_historic/<user>&<drog_name>&<hour>&<day>&<intake_respected>')
+class AddHistoricController(Resource):
+    @staticmethod
+    def post(user, drog_name, hour, day, intake_respected):
+        add_historic(user, drog_name, hour, day, intake_respected)
+        return {"creation": "DONE"}
