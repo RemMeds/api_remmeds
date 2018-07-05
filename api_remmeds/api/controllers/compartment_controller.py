7e3e6fd39eb6dd34394ab0dad6201f3efbb29ade
klougit <quentin.nicolas.pro@gmail.com>
2018-07-05 20:32:07 +0200

    create_account update

--- api_remmeds/api/controllers/compartment_controller.py
+++ api_remmeds/api/controllers/compartment_controller.py
@@ -1,15 +1,9 @@
 from flask_restplus import Resource, Namespace
 
-from api_remmeds.api.services.compartment_service import add_empty_compartment_new_account
+# from api_remmeds.api.services.compartment_service import add_empty_compartment_new_account
 
 ns = Namespace('compartment', description="Action on user's compartments")
 
-
-@ns.route('/add_empty_comp/<mail>', methods=['POST'])
-class EmptyCompController(Resource):
-    @staticmethod
-    def post(mail):
-        return add_empty_compartment_new_account(mail)
 
 # @ns.route(
 #     '/add_empty_comp/<mail>&<com_name>&<com_note>&<com_durationnumb>&<com_durationtext>&<com_check_bf>&<com_check_lun>&<com_check_din>&<com_check_bed>&<com_check_perso_hour>&<com_hour>&<com_frequency>&<com_mon>&<com_tue>&<com_wed>&<com_thu>&<com_fri>&<com_sat>&<com_sun>',
